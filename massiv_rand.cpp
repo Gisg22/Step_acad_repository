8ad35465b5c0889d47fdcf108b1eb243ef1349b4
Gisg <75253734+GodBoyDad@users.noreply.github.com>
2021-03-29 17:51:34 +0600

    Add files via upload

---
+++ massiv_rand.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <string>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	/**/int start_time = clock();
+
+
+	srand(time(NULL));
+	setlocale(0, "rus");
+	int arr[10];
+	int temp;
+	for (int i = 0; i < 10; i++) {
+		arr[i] = rand() % 10;
+		//cout << arr[i] << endl;
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (arr[i] > arr[j]) {
+				temp = arr[i];
+				arr[j] = arr[i];
+				arr[j] = temp;
+			}
+			else if (arr[i] < arr[j]){
+				temp = arr[j];
+				arr[i] = arr[j];
+				arr[i] = temp;
+				}
+		}
+
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << ", ";
+	}
+	cout << endl;
+
+	int end_time = clock();
+	int result = end_time - start_time;
+	cout << result << endl;
+
+	return 0;
+}
---
+++ users.cpp
@@ -0,0 +1,56 @@
+﻿// users.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+class users {
+public:
+
+private:
+	string name;
+	int department;
+	int data_birthday;
+	int data_start;
+};
+
+class admin :public users {
+public:
+
+private:
+
+};
+
+class moderator :public users {
+public:
+
+private:
+
+};
+
+class sales : public users {
+public:
+
+private:
+
+};
+
+class logistics : public users {
+public:
+
+private:
+
+};
+
+class clearing : public users {
+public:
+
+private:
+	 
+};
+
+int main()
+{
+  
+}
+
