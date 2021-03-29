c3d481a67abef364b2cbdd42fabc6f9adb53f18d
Gisg <75253734+GodBoyDad@users.noreply.github.com>
2021-03-29 15:30:50 +0600

    Add files via upload

---
+++ 0_64ui.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+
+
+
+
+
+
+
+
+
+
+    /*const int SIZE = 5;
+    const int SIZE_2 = 2;
+    string student_1_group[SIZE] = { ("Jamala", "Danial", "Fahur", "Khalia", "Shiva") };
+    string student_2_group[SIZE_2] = { ("Russian", "Indian") };
+    string true_name;
+    cout << "Введите имя: " << endl;
+    cin >> true_name;
+    int a = 0;
+        for (int i = 0; i < SIZE; i++) {
+            if (student_1_group[i] == true_name) {
+                a = 1;
+            }
+            else if (student_2_group[i] != true_name) {
+                a = 2;
+            }
+            if (a == 0)cout << "Нет такого имени." << endl;
+            else if (a == 1)cout << "Студенты 1." << endl;
+            else if (a == 2)cout << "Студенты 2." << endl;
+        } */
+}
