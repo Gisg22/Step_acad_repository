6d04550eabda7fd8d5e2e7ea3f23ed1ec3ee7ad6
Gisg <75253734+GodBoyDad@users.noreply.github.com>
2021-05-29 17:42:42 +0600

    Add files via upload

---
+++ massiv.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+   const int size = 5;
+   int shift;
+   int* arr = new int[size];
+   srand(time(0));
+   
+
+
+   for (int i = 0; i < size; i++) {
+	   arr[i] = rand() % 11;
+   }
+   cout << "NotShift: " << endl;
+   for (int i = 0; i < size; i++) {
+	   cout << arr[i] << endl;
+   }
+   int null_element = arr[0];
+   for (int i = 1; i < size; i++) {
+	   arr[i - 1] = arr[i];
+   }
+   arr[size - 1] = null_element;
+   cout << "Shift: " << endl;
+   for (int i = 0; i < size; i++) {
+	   cout << arr[i] << endl;
+   }
+   
+
+}
+
